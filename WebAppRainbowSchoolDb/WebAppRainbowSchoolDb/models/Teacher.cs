3ba19bb1f5669dab7936d6072787c3f501052b45
RohitKokare810 <Rohitkokare810@gmail.com>
2024-01-03 17:40:55 +0530

    initial commit

---
+++ WebAppRainbowSchoolDb/WebAppRainbowSchoolDb/models/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAppRainbowSchoolDb.models
+{
+    public partial class Teacher
+    {
+        public Teacher()
+        {
+            Marks = new HashSet<Mark>();
+        }
+
+        public int TeacherId { get; set; }
+        public string TeacherName { get; set; } = null!;
+
+        public virtual ICollection<Mark> Marks { get; set; }
+    }
+}
